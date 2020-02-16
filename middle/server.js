d5c9743eae7d3c10a3ad4c8d9a9f3a21f6162e63
otmichael <ozmichaelt@icloud.com>
2020-02-16 18:55:14 +0100

    lol

--- middle/server.js
+++ middle/server.js
@@ -6,17 +6,14 @@ const port = 8080;
 const path = require("path");
 var server = http.createServer(app);
 const io = require("socket.io")(server);
-let clients = new Set()
+let clients = [];
 
-io.on('connection', client => {
-	clients.add(client.id);
-	console.log(client.id + ' connected.');
-	console.log('Currently connected clients: ' + clients.values())
-	client.on('entities', data => {
-		console.log(data);
+io.on("connection", client => {
+	clients.push(client.id);
+	console.log("CLIENTS CONNECTED: " + clients);
+	client.on("entities", data => {
 	});
-	client.on('frame', data => {
-		console.log(data)
+	client.on("frame", data => {
 	});
 });
 
