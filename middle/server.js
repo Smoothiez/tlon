748b1288d04ff76299bffbb91502e5384fb62386
otmichael <ozmichaelt@icloud.com>
2020-02-16 21:56:32 +0100

    lol

--- middle/server.js
+++ middle/server.js
@@ -26,7 +26,7 @@ send.on("connection", client => {
 	});
 
 	client.on("frame", frame => {
-		console.log(frame)
+		console.log("lol");
 		for (var id in receivers) {
 			if (receivers[id] != client.handshake.query.token) {
 				console.log("ID " + client.id + " SENDING TO " + id);
